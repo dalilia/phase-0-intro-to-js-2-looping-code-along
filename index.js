77d661dd62a824b85a8e2b7281789f6814227a50
dalilia <dalila200875yahoo.fr>
2023-07-23 10:02:07 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+  function countDown(a) {
+    if (a <= 0) {
+      console.log("Please provide a positive integer.");
+      return;
+    }
+  
+    while (a >= 0) {
+      console.log(a);
+      a--;
+    }
+  }
+  countDown(10);
